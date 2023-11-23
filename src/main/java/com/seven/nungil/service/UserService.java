78e0b9b86dcc1a4628eae340d9b1613c6c006444
parkcoldroad <coldstreet@naver.com>
2023-11-23 21:16:35 +0900

    feat: 추천 위치 반환 시 퀴즈 존재 여부(isQuiz) 추가

--- src/main/java/com/seven/nungil/service/UserService.java
+++ src/main/java/com/seven/nungil/service/UserService.java
@@ -83,11 +83,12 @@ public class UserService {
                         place.getLatitude(),
                         place.getLongitude(),
                         place.getPlaceName(),
-                        place.getPlaceProvider()))
+                        place.getPlaceProvider(),
+			    		place.getQuiz() != null))
                 .collect(Collectors.toList());
     }
 
-    /**
+    /**x
      *  특정 추천 위치를 반환하는 메서드이다.
      *
      * @param placeId 추천 위치 id
@@ -96,6 +97,7 @@ public class UserService {
     public PlaceResponse getPlace(Long placeId){
         RecommendedPlace place = placeRepository.findById(placeId)
                 .orElseThrow(()-> new NotFoundException("Place Not Found " + placeId));
-        return new PlaceResponse(place.getPlaceId(),place.getLatitude(),place.getLongitude(), place.getPlaceName(), place.getPlaceProvider());
+        return new PlaceResponse(place.getPlaceId(),place.getLatitude(),place.getLongitude(), place.getPlaceName(),
+            place.getPlaceProvider(), place.getQuiz() != null);
     }
 }
